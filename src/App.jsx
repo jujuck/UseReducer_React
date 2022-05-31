c4b638bba2b134e7a0b4ddadd1972e42988d2e8b
Julien ichard WCS <julien.y.richard@gmail.com>
2022-05-31 14:35:18 +0200

    Adding a state in the App.jsx in order to manage our data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import react from 'react';
+import react, { useState } from 'react';
 import tasks from './data/tasks';
 import './App.css';
 import ToDoForm from './components/ToDoForm';
 import ToDoCard from './components/ToDoCard';
 
 function App() {
-
+  const [todos, setTodos] = useState(tasks)
   return (
     <div className="App">
       <h1 className="text-center">My To Do List App for better learning</h1>
       <ToDoForm />
       <div className="container">
-        {tasks.map(task => <ToDoCard task={task} key={task.label} />)}
+        {todos.map(task => <ToDoCard task={task} key={task.label} />)}
       </div>
     </div>
   )
